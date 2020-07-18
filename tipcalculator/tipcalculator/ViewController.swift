4e734e5f8b39da45c206af36cabcc27871e68c0d
Zainab <zalibhai2121@gmail.com>
2020-07-18 10:36:47 -0500

    change

---
+++ tipcalculator/tipcalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipcalculator
+//
+//  Created by Zainab Alibhai on 7/18/20.
+//  Copyright © 2020 Zainab Alibhai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
